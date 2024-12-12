49127521f67f3d2457a6ec85882043849c77bb16
SBC-HDL <hungdl@bsscommerce.com>
2024-12-12 17:52:06 +0700

    17:51 12/12

--- src/components/layout/MenuItem.tsx
+++ src/components/layout/MenuItem.tsx
@@ -7,7 +7,7 @@ type Props = {
 const MenuItem = ({ title, url, icon }: Props) => {
   return (
     <li>
-      <a href={url} className='p-3 rounded-md flex items-center gap-3'>
+      <a href={url} className='p-3 rounded-md flex items-center gap-3 text-gray70 hover:text-primary hover:bg-primary hover:bg-opacity-10 '>
         {icon}
         {title}
       </a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'gray70': '#70767f',
+        'primary': '#2c8fff'
       },
     },
   },
